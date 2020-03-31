8bb4b82678e4b446ba4dce57ad54fd8e16d73045
Jameson Campbell <jcampbell@flexterapp.com>
2020-03-30 19:10:58 -0500

    data and server file update

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
-app.get('/express_backend', (req, res) => {
-    return res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
-
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// app.get('/express_backend', (req, res) => {
+//     return res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 // });
 
-app.get('/ping', function (req, res) {
-    return res.send('pong');
-});
+// app.get('/ping', function (req, res) {
+//     return res.send('pong');
+// });

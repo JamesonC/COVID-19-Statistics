7f0d864b04c6bcf28f4a99f6f60e6c261c2ed4f9
Jameson Campbell <jcampbell@flexterapp.com>
2020-03-31 20:36:11 -0500

    make it better

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const port = process.env.PORT || 5005;
 const bodyParser = require('body-parser')
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'build', 'index.html')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
